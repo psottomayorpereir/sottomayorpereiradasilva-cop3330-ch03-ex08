c54fa8c56ecaee8d21e8c515d5db57b6b3d45b72
Pedro Henrique Sotto-Mayor Pereira da Silva <itapeu16@hotmail.com>
2021-11-28 20:49:44 -0800

    Added comments

--- main.cpp
+++ main.cpp
@@ -2,15 +2,23 @@
 using namespace std;
 
 int main() {
+  //declare variable
   int n;
-
+  //print message to ask for input
   cout << "Enter an integer: ";
+  //get user input
   cin >> n;
-
-  if ( n % 2 == 0)
+  //check if the number is even or odd by checking the remainder when dividing the number by 0
+  if ( n % 2 == 0){
+    //number is even
+    //display message
     cout << "The value " << n << " is an even number.";
-  else
+  }
+  else{
+    //number is odd
+    //display message
     cout << "The value " << n << " is an odd number.";
-
+  }
+  //return 0
   return 0;
 }
